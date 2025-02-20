05103e4d490f468de5bd79061863bd36ceff577b
MarianaGoncalves <marianas.g@live.com.pt>
2025-02-20 16:16:16 +0000

    First setup: add the first relevant libs (hilt, compose navigation,...), add application for hilt

---
+++ app/src/main/java/com/mariana/swordcatchallenge/SwordCatChallengeApplication.kt
@@ -0,0 +1,7 @@
+package com.mariana.swordcatchallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SwordCatChallengeApplication :Application()
---
+++ app/src/main/java/com/mariana/swordcatchallenge/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.mariana.swordcatchallenge.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Gray = Color(0xFFF6F4F2)
+val Black = Color(0xFF20222C)
+val White = Color(0xFFFFFFFF)
